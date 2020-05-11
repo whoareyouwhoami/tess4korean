06d7e6f3610e0ce955d77e4f9c7eab8c1f5a3aba
whoareyouwhoami <young.woo@hotmail.com>
2020-05-12 00:10:10 +0900

    minor fix

--- 1_generate_data.sh
+++ 1_generate_data.sh
@@ -13,5 +13,5 @@ $TRAIN_BASE_DIR/tesseract/src/training/tesstrain.sh --fonts_dir fonts \
 	     --langdata_dir $LANGDATA_DIR \
 	     --tessdata_dir $TRAIN_BASE_DIR/tesseract/tessdata \
 	     --save_box_tiff \
-	     --maxpages 10 \
+	     --maxpages $MAX_PAGES \
 	     --output_dir $LANG_TRAIN_DIR
